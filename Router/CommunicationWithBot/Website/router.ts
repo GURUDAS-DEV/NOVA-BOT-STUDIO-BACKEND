4c48a235fe3b6141b8c15508ca13ef7787b3166a
Gurudas Bhardwaj <gursad5@gmail.com>
2026-02-11 15:00:28 +0530

    Improvised and implemented Rate limiting

--- Router/CommunicationWithBot/Website/router.ts
+++ Router/CommunicationWithBot/Website/router.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { accessMiddleware } from "../../../Middleware/accessMiddleware.js";
 import { controlledStyleWebsiteBotController, freestyleWebsiteBotController } from "../../../controller/BotCommunication/Website/controllert.js";
+import { LimitBotCommunication } from "../../../Middleware/Rate_Limiting/LimitBotCommunication.js";
 
 
 const communcationWithBotRouter = Router();
 
-communcationWithBotRouter.post("/FreeStyleChat", accessMiddleware, freestyleWebsiteBotController);
-communcationWithBotRouter.post("/ControlledStyleChat", accessMiddleware, controlledStyleWebsiteBotController)
+communcationWithBotRouter.post("/FreeStyleChat" , accessMiddleware, LimitBotCommunication, freestyleWebsiteBotController);
+communcationWithBotRouter.post("/ControlledStyleChat", accessMiddleware,LimitBotCommunication, controlledStyleWebsiteBotController)
 
 export default communcationWithBotRouter;
